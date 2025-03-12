ba62faf5f34d5867cd54c29adbe11a3f98b5c80d
tamiz08 <tamilsrt13@gmail.com>
2025-03-12 23:43:07 +0530

    Update helloworld.java

--- src/main/java/basic/helloworld.java
+++ src/main/java/basic/helloworld.java
@@ -2,7 +2,7 @@ package basic;
 
 public class helloworld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // "from branch b11"
 		System.out.println("hello world!!!");
 
 	}
